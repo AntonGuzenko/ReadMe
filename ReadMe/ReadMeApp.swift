76e51a1c73b11ae2653aea59b9527d8acdc214c9
AntonGuzenko <guzik8989@gmail.com>
2021-12-28 21:37:12 +0200

    Initial Commit

---
+++ ReadMe/ReadMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReadMeApp.swift
+//  ReadMe
+//
+//  Created by Антон Гузенко on 28.12.2021.
+//
+
+import SwiftUI
+
+@main
+struct ReadMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
