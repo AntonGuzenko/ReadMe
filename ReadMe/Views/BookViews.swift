68e3ee0b96b650247c995de04c9b30ac8b5cc22f
AntonGuzenko <guzik8989@gmail.com>
2021-12-28 23:22:12 +0200

    added details

--- ReadMe/Views/BookViews.swift
+++ ReadMe/Views/BookViews.swift
@@ -7,9 +7,29 @@
 
 import SwiftUI
 
+struct TitleAndAuthorStack: View {
+    let book: Book
+    let titleFont: Font
+    let authorFont: Font
+    
+    var body: some View {
+        VStack(alignment: .leading) {
+            Text(book.title)
+                .font(titleFont)
+            Text(book.author)
+                .font(authorFont)
+                .foregroundColor(.secondary)
+            
+        }
+    }
+}
+
+
 extension Book {
     struct Image: View {
         let title: String
+        var size: CGFloat?
+        
         var body: some View {
             let symbol = SwiftUI.Image(title: title)
             ?? .init(systemName: "book")
@@ -17,7 +37,7 @@ extension Book {
             symbol
                 .resizable()
                 .scaledToFit()
-                .frame(width: 80, height: 80)
+                .frame(width: size, height: size)
                 .font(Font.title.weight(.light))
                 .foregroundColor(.secondary)
         }
